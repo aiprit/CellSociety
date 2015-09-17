d30627cb8d1f3993462d1acaac930ef902ea5173
Rob Martorano (netid: rfm21) <rfm21@duke.edu>
2015-09-17 15:27:01 -0400

    update ultra basics of ui

--- src/cellsociety_team10/Graphic_Controller.java
+++ src/cellsociety_team10/Graphic_Controller.java
@@ -2,7 +2,7 @@ package cellsociety_team10;
 
 import java.util.ArrayList;
 
-import javafx.application.*;
+import javafx.scene.paint.Color;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.layout.*;
@@ -10,20 +10,36 @@ import javafx.scene.control.*;
 
 public class Graphic_Controller {
 	private static final String APPLICATION_NAME = "Cell Society";
-	public static final int SCREEN_WIDTH = 800;
-	public static final int SCREEN_HEIGHT = 800;
+	public static final int SCREEN_WIDTH = 700;
+	public static final int SCREEN_HEIGHT = 700;
 	private Stage current_stage;
 	private ArrayList<Scene> simulations;
 	private Scene current_scene;
+	private Group root;
+	private User_Interface ui;
 
 	public Graphic_Controller(Stage main_stage){
 		current_stage = main_stage;
+		root = new Group();
+		ui= new User_Interface();
+		set_up_scene();
+
 	}
 
-	public void set_up_scene(){
-		current_stage.setScene(current_scene);
-	    current_stage.setTitle(APPLICATION_NAME);
-	    current_stage.show();
+	public void set_up_scene() {
+
+		current_stage.setScene(scene_creation());
+		current_stage.setTitle(APPLICATION_NAME);
+		current_stage.show();
+
+		ui.set_up_base_scene(root);
+
+	}
+
+	public Scene scene_creation(){
+		current_scene = new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT, Color.GREY);
+		root.getStylesheets().add("/Stylesheet.css");
+		return current_scene;
 	}
 
 
--- src/cellsociety_team10/User_Interface.java
+++ src/cellsociety_team10/User_Interface.java
@@ -1,5 +1,36 @@
 package cellsociety_team10;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
 public class User_Interface {
-	
+    private Group basic_scene;
+    private BorderPane outer_format;
+    private VBox control_panel;
+    private Label title;
+    private GridPane grid_view;
+
+
+    public User_Interface(){
+        control_panel = new VBox();
+        grid_view = new GridPane();
+        title = new Label("CellSociety");
+    }
+
+    public void set_up_base_scene(Group basic){
+        outer_format = new BorderPane();
+        control_panel = new VBox();
+        outer_format.setRight(control_panel);
+        outer_format.setLeft(grid_view);
+        outer_format.setTop(title);
+        basic.getChildren().add(outer_format);
+    }
+
+
+
+
 }
