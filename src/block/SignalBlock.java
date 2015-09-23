76a9b5d88145426d575a39ce5797d975b8d3320f
Nicky Groszewski <groszewn@gmail.com>
2015-09-22 23:31:20 -0400

    slime mold simulation

---
+++ src/block/SignalBlock.java
@@ -0,0 +1,16 @@
+package block;
+
+import javafx.scene.paint.Color;
+
+public class SignalBlock extends Block {
+	
+	private Color blockColor = Color.WHITE;
+
+
+
+	
+	public void act() {
+		
+		
+	}
+}
